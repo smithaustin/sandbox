683f81c9399cd9fb20eb72f368ca17b41b2aaa52
Austin <austinsmith@uvic.ca>
2019-08-26 00:00:07 -0700

    got it working to output to console

--- getMostAbundantTran.py
+++ getMostAbundantTran.py
@@ -5,20 +5,48 @@ import re
 
 codebook = pandas.read_csv("codebook.csv", header=None)
 valid_trs = pandas.read_csv("valid_trs.csv", names=["Name", "ID"])
-abundance = pandas.read_csv("./../abundance/abundance.tsv", sep='\t')
+abundance = pandas.read_csv("abundance.tsv", sep='\t')
 
 size = codebook.shape
 codebook = codebook[4:]
+
+count = 0
 
 for (i, name) in enumerate(codebook[0]):
     if re.match(r"(?:b|B)lank", name) is not None:
         continue
     
-    print(valid_trs[valid_trs['Name'].str.match('name')])
+    # if count < 10:
+    #     count += 1
+    # else:
+    #     break
+    gene_transcripts = valid_trs[valid_trs['Name'] == name]
     
+    if len(gene_transcripts) == 0:
+        print(("-------------------------------------------NO VALID TRS FOR " + name + '\n')*5)
+        continue
+
+    gene_transcripts_ids = gene_transcripts['ID'].str.slice(0,15,1)
+    # print(gene_transcripts_ids)
+
+    transcript_abundance = []
+    transcript_ids_list = []
+    print(name)
+    for id in gene_transcripts_ids:
+        if len(abundance[abundance['target_id'].str.match(id)]['tpm']) < 1:
+            print(("----------------------------could not find id " + id + '\n')*3)
+            continue
+        transcript_abundance.append(float(abundance[abundance['target_id'].str.match(id)]['tpm']))
+        transcript_ids_list.append(id)
+    
+    print(transcript_abundance)
+    idx = int(np.argmax(transcript_abundance))
+    print('go with position ' + str(idx))
+    print(transcript_ids_list[idx])
+    print("______________________________")
 
 
 
-print(codebook)
-print(valid_trs)
-print(abundance)
+# print(codebook)
+# print(valid_trs)
+# print(abundance)
